ad64abc31b2cb70d0e9a351af3b599a62972e878
Roland Tanglao <roland@rolandtanglao.com>
2017-11-19 20:36:43 -0800

    only loads first img, doesn't load all the other images

--- downloadMyLDPhotos.rb
+++ downloadMyLDPhotos.rb
@@ -1,10 +1,7 @@
 #!/usr/bin/env ruby
 require 'rubygems'
 require 'capybara/poltergeist'
-
-require 'phantomjs'
-require 'capybara/dsl'
-include Capybara::DSL
+require 'pp'
 
 # Configure Poltergeist to not blow up on websites with js errors aka every website with js
 # See more options at https://github.com/teampoltergeist/poltergeist#customization
@@ -14,12 +11,20 @@ include Capybara::DSL
 Capybara.register_driver :poltergeist do |app|
   Capybara::Poltergeist::Driver.new(
     app, timeout: 180, port: 8910, host: "127.0.0.1",
-    debug: true,
-    phantomjs_options: ['--ignore-ssl-errors=yes', '--ssl-protocol=any', '--load-images=no']
+    #debug: true,
+    js_errors: false,
+    phantomjs_options: ['--ignore-ssl-errors=yes', '--ssl-protocol=any']
+                        #, '--load-images=no']
     #, phantomjs_options: ['--load-images=no]
                                    )
 end
 # Configure Capybara to use Poltergeist as the driver
 Capybara.default_driver = :poltergeist
-page.driver.options[:phantomjs] = Phantomjs.path # MAGIC
-
+#page.driver.options[:phantomjs] = Phantomjs.path # MAGIC
+browser = Capybara.current_session
+url = "https://photolab.londondrugs.com/prints?coll=mVnr3ZX2GN3v1K46YLzyJxjo"
+browser.visit url
+# browser.save_and_open_page
+browser.all('img').each do |i|  
+  pp i[:src]
+end
